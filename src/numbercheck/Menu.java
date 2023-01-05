c04129e3a902c032e9734daa1dc63c2a0385d95c
Otipst <pitokhirahman@gmail.com>
2023-01-05 14:46:31 +0700

    commit 6

--- src/numbercheck/Menu.java
+++ src/numbercheck/Menu.java
@@ -12,47 +12,51 @@ package numbercheck;
 import java.util.Scanner;
 public class Menu
 {
-    public void Menu()
-    {
-	int option;
-        Scanner sc = new Scanner(System.in);	
+ public void Menu()
+ {
+ int option;
+ Scanner sc = new Scanner(System.in);
+ System.out.println("---------Menu---------");
+ System.out.println("1. Check Odd or Even");
+ System.out.println("2. Check Prime");
+ System.out.println("3. Exit");
+ System.out.print("\nChoose the option: ");
 
-        System.out.println("---------Menu---------");
-        System.out.println("1. Check Odd or Even");
-        System.out.println("2. Exit");
-        System.out.print("\nChoose the option: ");
-                
-        option = sc.nextInt();
-	
-	// Switch Case
-        
-        switch(option)
-        {
-            case 1: CheckOddOrEven();
-                    break;
-            case 2: Exit();
-                    break;
-            default: System.out.println("Incorrect menu option");
-                     Menu();
-                    break;
-       }
-    	   
-    }
+ option = sc.nextInt();
 
-    // Methods definition
-    public void CheckOddOrEven() 
-    {
-	OddOrEven obj = new OddOrEven();
-        obj.CheckOddOrEven();
-    }
+ // Switch Case
 
-     public void Exit() 
-    {
-        System.exit(0);
-    } 
-public static void main(String[] args)
-    {
-      Menu obj = new Menu();
-        obj.Menu();
-    }
-    }
+ switch(option)
+ {
+ case 1: CheckOddOrEven();
+ break;
+ case 2: CheckPrimeNumber();
+ break;
+ case 3: Exit();
+ break;
+ default: System.out.println("Incorrect menu option");
+ Menu();
+ break;
+ }
+ } 
+ // Methods definition
+ public void CheckOddOrEven()
+ {
+ OddOrEven obj = new OddOrEven();
+ obj.CheckOddOrEven();
+ }
+ public void CheckPrimeNumber()
+ {
+ CheckPrime obj = new CheckPrime();
+ obj.CheckPrime();
+ }
+ public void Exit()
+ {
+ System.exit(0);
+ }
+ public static void main(String[] args)
+ {
+ Menu obj = new Menu();
+ obj.Menu();
+}
+ } 
